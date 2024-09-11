8bc55af3e31a8ec43828e1cf1df8b0d5ca0d698a
Glauco Gonçalves <glaucogoncalves@ufpa.br>
2024-09-11 10:43:12 -0300

    removing auth from rest server

--- rest/rest-server.py
+++ rest/rest-server.py
@@ -1,9 +1,6 @@
-#!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
-#from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__, static_url_path="")
-#auth = HTTPBasicAuth()
 
 
 @app.errorhandler(400)
